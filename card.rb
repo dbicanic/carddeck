481e423e3290847ff4fbff191b66b52ba3a39a80
dbicanic <dbicanic77@gmail.com>
2016-09-21 14:03:56 -0500

    Have face cards working

--- card.rb
+++ card.rb
@@ -2,12 +2,23 @@ class Card
 	attr_reader :rank, :suit
 
 	def initialize(rank, suit)
-		@rank = rank
+		@rank = face_converter(rank)
 		@suit = suit
 	end
 
 	def face_converter(rank)
-		
+		case rank
+		when 14
+			@rank = "Ace"
+		when 13
+			@rank = "King"
+		when 12
+			@rank = "Queen"
+		when 11 
+			@rank = "Jack"
+		else
+			@rank = rank
+		end
 	end
 
 end
