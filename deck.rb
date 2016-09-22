95355883d53de3c90b3e242bcab9362888429bf4
dbicanic <dbicanic77@gmail.com>
2016-09-22 14:46:20 -0500

    Test for number of cards, and if it includes a random card object

--- deck.rb
+++ deck.rb
@@ -1,3 +1,21 @@
 class Deck
+	attr_reader :deck
+
+	def initialize
+		@deck = create
+	end
+
+	def create
+		r = 2
+		@deck = []
+		until r == 15 do 
+			@deck << Card.new(r, "Spades")
+			@deck << Card.new(r, "Diamonds")
+			@deck << Card.new(r, "Clubs")
+			@deck << Card.new(r, "Hearts")
+			r += 1
+		end
+		@deck
+	end
 
 end
--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -2,8 +2,14 @@ require_relative "../deck"
 
 describe Deck do 
 	let(:deck) {Deck.new}
+	let(:ace_card) {Card.new(14, "Diamonds")}
 
 	it "should have 52 cards" do 
-		expect(deck.length).to eq 52
+		expect(deck.create.length).to eq 52
 	end
+
+	it "contains a card object" do 
+		expect(deck.deck).to include(ace_card)
+	end
+
 end
