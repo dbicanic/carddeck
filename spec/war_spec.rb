46f29a74d3a1cf352e9d0870d3abc72ffbf46169
dbicanic <dbicanic77@gmail.com>
2016-09-23 11:16:46 -0500

    Create tests for war class

---
+++ spec/war_spec.rb
@@ -0,0 +1,12 @@
+require_relative "../war"
+
+describe War do 
+	it "draws two cards" do 
+	end
+
+	it "shows the user his/her card" do 
+	end
+
+	it "declares a winner" do 
+	end
+end
---
+++ war.rb
@@ -0,0 +1,7 @@
+class War
+	attr_reader :war
+	initialize
+		@war = []
+	end 
+
+end
